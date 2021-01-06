720106f58326427727dd38b859a4f414a682b595
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2021-01-06 10:48:29 +0530

    update skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,14 +2,26 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import { ChevronDoubleDown } from "react-bootstrap-icons";
 import "./GPCSS.css";
+import "./Skills.css";
 class Skills extends React.Component {
   render() {
     return (
       <div id="skills">
         <Card>
           <Card.Header className="heading">Skills</Card.Header>
-          <Card.Body></Card.Body>
-          <Card.Footer></Card.Footer>
+          <Card.Body>
+            <div className="container">
+              <div>C++</div>
+              <div>HTML</div>
+              <div>CSS</div>
+              <div>BootStrap-4</div>
+              <div>JavaScript</div>
+              <div>Python</div>
+              <div>React</div>
+              <div>Node</div>
+              <div>MySQL</div>
+            </div>
+          </Card.Body>
         </Card>
         <div
           style={{
