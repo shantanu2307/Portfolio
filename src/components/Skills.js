49e86b99ff096af46e1e1c0bef00a07a52dd7524
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2021-01-07 10:42:06 +0530

    skills updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import { ChevronDoubleDown } from "react-bootstrap-icons";
 import "./GPCSS.css";
-import "./Skills.css";
 class Skills extends React.Component {
   render() {
     return (
@@ -11,15 +10,236 @@ class Skills extends React.Component {
           <Card.Header className="heading">Skills</Card.Header>
           <Card.Body>
             <div className="container">
-              <div className="item">C++</div>
-              <div className="item">HTML</div>
-              <div className="item">CSS</div>
-              <div className="item">BootStrap-4</div>
-              <div className="item">JavaScript</div>
-              <div className="item">Python</div>
-              <div className="item">React</div>
-              <div className="item">Node</div>
-              <div className="item">MySQL</div>
+              <div
+                className="progress"
+                style={{
+                  border: "1px solid black",
+                  width: "40%",
+                  height: "40%",
+                }}
+              >
+                <div
+                  className="progress-bar progress-bar-striped progress-bar-animated"
+                  role="progressbar"
+                  aria-valuenow="95"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{
+                    width: "95%",
+                    backgroundColor: "#f5c4f0",
+                    color: "black",
+                  }}
+                >
+                  <b>CSS</b>
+                </div>
+              </div>
+              <div
+                className="progress"
+                style={{
+                  border: "1px solid black",
+                  width: "40%",
+                  height: "40%",
+                }}
+              >
+                <div
+                  className="progress-bar progress-bar-striped progress-bar-animated"
+                  role="progressbar"
+                  aria-valuenow="95"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{
+                    width: "90%",
+                    backgroundColor: "#d0f582",
+                    color: "black",
+                  }}
+                >
+                  <b>HTML</b>
+                </div>
+              </div>
+              <div
+                className="progress"
+                style={{
+                  border: "1px solid black",
+                  width: "40%",
+                  height: "40%",
+                }}
+              >
+                <div
+                  className="progress-bar progress-bar-striped progress-bar-animated"
+                  role="progressbar"
+                  aria-valuenow="95"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{
+                    width: "70%",
+                    backgroundColor: "#d0f582",
+                    color: "black",
+                  }}
+                >
+                  <b>BootStrap</b>
+                </div>
+              </div>
+              <div
+                className="progress"
+                style={{
+                  border: "1px solid black",
+                  width: "40%",
+                  height: "40%",
+                }}
+              >
+                <div
+                  className="progress-bar progress-bar-striped progress-bar-animated"
+                  role="progressbar"
+                  aria-valuenow="95"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{
+                    width: "95%",
+                    backgroundColor: "#f5c4f0",
+                    color: "black",
+                  }}
+                >
+                  <b>javaScript</b>
+                </div>
+              </div>
+              <div
+                className="progress"
+                style={{
+                  border: "1px solid black",
+                  width: "40%",
+                  height: "40%",
+                }}
+              >
+                <div
+                  className="progress-bar progress-bar-striped progress-bar-animated"
+                  role="progressbar"
+                  aria-valuenow="95"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{
+                    width: "95%",
+                    backgroundColor: "#f5c4f0",
+                    color: "black",
+                  }}
+                >
+                  <b>ReactJS</b>
+                </div>
+              </div>
+              <div
+                className="progress"
+                style={{
+                  border: "1px solid black",
+                  width: "40%",
+                  height: "40%",
+                }}
+              >
+                <div
+                  className="progress-bar progress-bar-striped progress-bar-animated"
+                  role="progressbar"
+                  aria-valuenow="95"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{
+                    width: "90%",
+                    backgroundColor: "#d0f582",
+                    color: "black",
+                  }}
+                >
+                  <b>NodeJS</b>
+                </div>
+              </div>
+              <div
+                className="progress"
+                style={{
+                  border: "1px solid black",
+                  width: "40%",
+                  height: "40%",
+                }}
+              >
+                <div
+                  className="progress-bar progress-bar-striped progress-bar-animated"
+                  role="progressbar"
+                  aria-valuenow="95"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{
+                    width: "85%",
+                    backgroundColor: "#d0f582",
+                    color: "black",
+                  }}
+                >
+                  <b>MongoDB</b>
+                </div>
+              </div>
+              <div
+                className="progress"
+                style={{
+                  border: "1px solid black",
+                  width: "40%",
+                  height: "40%",
+                }}
+              >
+                <div
+                  className="progress-bar progress-bar-striped progress-bar-animated"
+                  role="progressbar"
+                  aria-valuenow="95"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{
+                    width: "60%",
+                    backgroundColor: "#f5c4f0",
+                    color: "black",
+                  }}
+                >
+                  <b>Python</b>
+                </div>
+              </div>
+              <div
+                className="progress"
+                style={{
+                  border: "1px solid black",
+                  width: "40%",
+                  height: "40%",
+                }}
+              >
+                <div
+                  className="progress-bar progress-bar-striped progress-bar-animated"
+                  role="progressbar"
+                  aria-valuenow="95"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{
+                    width: "90%",
+                    backgroundColor: "#f5c4f0",
+                    color: "black",
+                  }}
+                >
+                  <b>C/C++</b>
+                </div>
+              </div>
+              <div
+                className="progress"
+                style={{
+                  border: "1px solid black",
+                  width: "40%",
+                  height: "40%",
+                }}
+              >
+                <div
+                  className="progress-bar progress-bar-striped progress-bar-animated"
+                  role="progressbar"
+                  aria-valuenow="95"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{
+                    width: "75%",
+                    backgroundColor: "#d0f582",
+                    color: "black",
+                  }}
+                >
+                  <b>MySQL</b>
+                </div>
+              </div>
             </div>
           </Card.Body>
         </Card>
