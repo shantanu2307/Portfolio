1f968553dfdfdcaea637b638aa646d25d61c16de
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2021-01-07 12:18:40 +0530

    Added Timeline

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { ChevronDoubleDown } from "react-bootstrap-icons";
+import { ChevronDoubleDown, ArrowUp } from "react-bootstrap-icons";
 import "./GPCSS.css";
 import "./Home.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 class AboutMe extends React.Component {
   constructor(props) {
     super(props);
@@ -29,10 +34,72 @@ class AboutMe extends React.Component {
               another adventure.
             </div>
             <br />
-            <br />
-            <div className="abt">Connect with Me!</div>
+            <h1 style={{ fontSize: "4vw", color: "#160457" }}>Timeline</h1>
+            <VerticalTimeline>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--education"
+                date="August 2019 - Present "
+                iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+                icon={<ArrowUp />}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  Netaji Subhas University of Technology, New Delhi
+                </h3>
+                <h4 className="vertical-timeline-element-subtitle">
+                  Bachelor of Technology
+                </h4>
+                <p>
+                  Computer Science and Engineering with Specialisation in
+                  Artificial Intelligence
+                </p>
+                <p>
+                  <b>CGPA: 9.0</b>
+                </p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--education"
+                date="2019"
+                iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+                icon={<ArrowUp />}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  Alhcon Public School, Mayur Vihar, New Delhi
+                </h3>
+                <h4 className="vertical-timeline-element-subtitle">
+                  CBSE AISSCE (PCM)
+                </h4>
+                <p>
+                  <b>
+                    12<sup>th</sup> Boards - 91.8%
+                  </b>
+                </p>
+                <p>
+                  <b>JEE Advanced: AIR 5304</b>{" "}
+                </p>
+                <p>
+                  <b>JEE Mains: AIR 5296</b>{" "}
+                </p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--education"
+                date="2017"
+                iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+                icon={<ArrowUp />}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  Amity International School, Noida
+                </h3>
+                <h4 className="vertical-timeline-element-subtitle">Class 10</h4>
+                <p>
+                  <b>CGPA: 10</b>
+                </p>
+              </VerticalTimelineElement>
+            </VerticalTimeline>
           </Card.Body>
-          <Card.Footer>{this.props.children}</Card.Footer>
+          <Card.Footer>
+            <div className="abt">Connect with Me!</div>
+            {this.props.children}
+          </Card.Footer>
         </Card>
         <div
           style={{
